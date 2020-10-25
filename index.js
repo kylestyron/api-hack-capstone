093ca521fdfc9e1dcd3f104ce0c5e80b594e4bf2
Kyle Styron <kylestyron@gmail.com>
2020-10-24 17:58:35 -0700

    worked on js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function createResultItem(card) { //append these to ul for each item returned by searches
-    let tcgPrice = 4.25;
-    let ebayPrice = 5.00;
+    const tcgCard = '';
+    const ebayCard = '';
+
+    console.log(getEbayInfo(card));
     
     return `<li class="result"><img src=${card.imageUrl} alt=${card.name} ${card.set} ${card.number}>
     <br>
@@ -23,16 +25,33 @@ function displayResults(response) {
 }
 
 function toPokemonUrl(params) {
-    const baseUrl= 'https://api.pokemontcg.io/v1/';
+    const baseUrl = 'https://api.pokemontcg.io/v1/';
     return baseUrl + `cards?name=${params}`;
 }
 
-function getEbayPrice(card) {
-    
+function getEbayInfo(card) {
+    const appID = 'KyleStyr-cards-PRD-0e64be0f2-f6e14bc0';
+    const url = `https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=KyleStyr-cards-PRD-0e64be0f2-f6e14bc0&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${card.name}+${card.set}&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-US&siteid=0`;
+
+    let requestOptions = {
+      method: 'GET',
+      mode: 'cors'
+    };
+
+    fetch(url, requestOptions)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
 }
 
-function getTcgplayerPrice(card) {
-
+function getTcgplayerInfo(card) {
+    const key = '';
 }
 
 function cardSearch(query) {
